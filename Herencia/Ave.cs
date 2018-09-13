679794758f862144a52e49f099dc9cf50845019a
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-13 10:41:40 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Ave.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Ave : Animal
+    {
+        public string ponerHuevo()
+        {
+            return Nombre + " puso un huevo";
+        }
+
+        override public string desplazar()
+        {
+            return Nombre + " está volando";
+        }
+    }
+}
---
+++ Herencia/Reptil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Reptil : Animal
+    {
+        public string cambiarPiel()
+        {
+            return Nombre + " cambió de piel";
+        }
+
+        override public string desplazar()
+        {
+            return Nombre + " está caminando";
+        }
+    }
+}
